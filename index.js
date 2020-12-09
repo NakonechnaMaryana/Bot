576f61b1b42cd2eaa037f245fba59aad97f18f8d
Maryana <mariananakonechna@gmail.com>
2020-12-09 13:19:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require('node-telegram-bot-api'); 
-const token = ${token_1};
+const token = `${token_1}`;
 
 const bot = new TelegramBot(token, {polling: true});
 
